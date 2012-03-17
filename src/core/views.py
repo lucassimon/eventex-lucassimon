9368125187ab733b093428b0251a19ec4e38d7af
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2012-03-17 16:21:52 -0300

    modulo comming soon

--- src/core/views.py
+++ src/core/views.py
@@ -3,7 +3,10 @@
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+import datetime
 
 def homepage(request):
+	dataAtual = datetime.datetime.now()
+	dataInscricao = datetime.date(2012,03,18)
 	context = RequestContext(request)
-	return render_to_response('index.html',context)
+	return render_to_response('index.html',locals(),context)
