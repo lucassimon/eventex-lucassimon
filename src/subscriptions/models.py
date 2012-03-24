df7ae417c21315dd35a08ececc9eb2a02a87c067
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2012-03-24 18:38:11 -0300

    Adicionado comentario no atributo created_at

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -8,4 +8,5 @@ class Subscription(models.Model):
 	cpf        = models.CharField(max_length=11, unique=True)
 	email      = models.EmailField(unique=True)
 	phone      = models.CharField(max_length=20, blank=True)
+	""" Gravar com a data e hora atual """
 	created_at = models.DateTimeField(auto_now_add=True)
