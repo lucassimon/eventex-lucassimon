6b75863dbf69ed09dcc54b37f0d802a4adef6d2d
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2012-03-24 22:40:04 -0300

    Consertado a classe estava dando erro no runserver

--- src/subscriptions/forms.py
+++ src/subscriptions/forms.py
@@ -2,8 +2,9 @@ from django import forms
 from subscriptions.models import Subscription
 
 
-
-class SubscriptionForm(forms.ModelForm)
-   	class Meta:
-		model   = Subscription
+class SubscriptionForm(forms.ModelForm):
+	"""docstring for SubscriptionForm"""
+	class Meta:
+		"""docstring for Meta"""
+		model = Subscription
 		exclude = ('created_at',)
