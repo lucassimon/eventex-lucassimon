efc80d8d5bf8b8801931b1ce038ad5194cd5522e
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2012-03-25 00:56:17 -0300

    adicionado os definicoes para admin

--- src/urls.py
+++ src/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls.defaults import patterns, include, url
 from core.views import homepage
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
+
+
 
 urlpatterns = patterns('django.views.generic.simple',
     # Examples:
@@ -15,10 +18,11 @@ urlpatterns = patterns('django.views.generic.simple',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include('django.contrib.admin.urls')),
 
     # Generalizando as views passando como parametro qual template vou usar
-    url(r'^$',homepage, {'template': 'index.html'}),
+    url(r'^$','direct_to_template', {'template': 'index.html'}),
 
     #url(r'^$', 'direct_to_template', {'template':'index.html'})
     url(r'^inscricao/',include('subscriptions.urls',namespace='subscriptions')),
