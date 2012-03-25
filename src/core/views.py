81d742fdbf780b1a8227750e503f0f6634b456a8
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2012-03-24 21:30:20 -0300

    alterado a action/metodo home page para exibir de acordo com  uma view generica

--- src/core/views.py
+++ src/core/views.py
@@ -5,8 +5,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 import datetime
 
-def homepage(request):
+def homepage(request,template=None):
+	""" Action para exibir a home do site """
 	dataAtual = datetime.datetime.now()
 	dataInscricao = datetime.datetime(2012,03,18,9,00,00,00)
 	context = RequestContext(request)
-	return render_to_response('index.html',locals(),context)
+	return render_to_response(template,locals(),context)
