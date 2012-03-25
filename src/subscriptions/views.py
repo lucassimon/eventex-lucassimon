0616775f51ed975f45aa6ada056e72879fe147ae
Lucas Simon Rodrigues Magalhaes <lucassrod@gmail.com>
2012-03-24 21:28:48 -0300

    Criada a logica para construir o fformulario de subscricao de eventos

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,1 +1,11 @@
 # Create your views here.
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from forms import SubscriptionForm
+
+def subscribe(request):
+	""" Action para exibir o formulario para o usuario """
+	form=SubscriptionForm()
+	context = RequestContext(request, {'form': form})
+	return render_to_response('subscriptions/new.html',context)
